36e79bab0981e4b85a55dd6aac7bf1874f6ff9e0
Rhianne Price <price.rhianne@gmail.com>
2017-04-26 22:09:23 +1200

    add chromosome class

---
+++ cosc343worldJava/Chromosome.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Chromosome {
+  public int preferenceForMonsters;
+  public int preferenceForFood;
+  public int preferenceForRed;
+  public int[] preferenceForEating;
+  public int preferenceForFriends;
+
+  public Chromosome() {
+    
+  }
+
+  public Chromosome(Chromosome mum, Chromosome dad) {
+
+  }
+}
